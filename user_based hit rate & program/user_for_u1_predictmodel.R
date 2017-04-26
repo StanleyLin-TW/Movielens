7d8829697e3a2d363134f3a233081daeace9411f
AndreChang85 <ianchang0815@gmail.com>
2017-04-27 00:47:01 +0800

    Finally_output by Andre
    我終於完成輸出了 <3

--- user_based hit rate & program/user_for_u1_predictmodel.R
+++ user_based hit rate & program/user_for_u1_predictmodel.R
@@ -10,18 +10,19 @@ library(recommenderlab)
 ratingmat <- as(ratingmat, "realRatingMatrix")
 ratingmat_norm <- normalize(ratingmat)
 recommender_model <- Recommender(ratingmat_norm, method = "UBCF", param=list(method="Cosine",nn=30)) 
-recom_results=rep(0,943,1682)
+recom_results=matrix(0,943,1682)
 for(k in 1:943){
  	recom <- predict(recommender_model, ratingmat[k], type = "ratings") 
  	recom_pre <- as(recom, "matrix")
  	for(i in 1:ncol(recom_pre)){
- 		if(is.na(recom_pre[i])){
- 			recom_pre[i]=0
+ 		if(is.na(recom_pre[1,i])){
+ 			recom_pre[1,i]=0
  		}	
  	}
  	recom_result <- matrix(0,1682)
  	for(i in 1:ncol(recom_pre)){
- 		recom_result[i] <- recom_pre[1,i]
+ 		j = as.integer(colnames(recom_pre)[i])
+ 		recom_result[j] <- recom_pre[1,i]
  	}
  	for(i in 1:1682){
  		recom_results[k,i] <- recom_result[i]
@@ -29,7 +30,6 @@ for(k in 1:943){
  	
 }
 
-library(MASS)
-write.matrix(recom_results, file = "u1base_predict.csv")
+write.csv(recom_results, file = "u1base_predict.csv")
  
 #write.csv(recom_results,file="u1base_predict.csv")
